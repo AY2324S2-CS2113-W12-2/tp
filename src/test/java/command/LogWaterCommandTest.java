a01e1da8dc9869612497b39335b6d61f77eead13
Praneet-25 <e0959984@u.nus.edu>
2024-04-01 12:42:13 +0800

    no message

--- src/test/java/command/LogWaterCommandTest.java
+++ src/test/java/command/LogWaterCommandTest.java
@@ -1,9 +1,7 @@
 package command;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import java.time.LocalDateTime;
 
 

3a6a7564e1c80042933801837cd0116a2c9a8bb9
nikhil-2101 <nikhilaaditya.babu@gmail.com>
2024-04-08 17:18:47 +0800

    fixing bugs for log meal command

--- src/main/java/command/LogMealCommand.java
+++ src/main/java/command/LogMealCommand.java
@@ -33,8 +33,8 @@ public class LogMealCommand {
                 tasksList.add(logMeal);
                 CommandUi.printMealLogMessage(logMeal);
                 if (exceedsCalorieGoal(totalCaloriesConsumed)) {
-                        CommandUi.printCalorieExceedingWarning();
-                    }
+                    CommandUi.printCalorieExceedingWarning();
+                }
             } else {
                 CommandUi.printFoodItemNotFoundMessage(description);
             }

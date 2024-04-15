7260333ffc458d9e97bb3dcd7ffb4f3668a896fe
Nikhil <nikhilaaditya.babu@gmail.com>
2024-04-15 17:22:54 +0800

    documentation and PPP

--- src/main/java/command/InvalidCommand.java
+++ src/main/java/command/InvalidCommand.java
@@ -1,9 +1,25 @@
 package command;
+
+/**
+ * Represents an invalid command. This command is used to handle situations
+ * where the input command is not recognized or cannot be executed.
+ * When executed, this command prints the specified error message to the console.
+ */
 public class InvalidCommand extends Command {
     private String errorMessage;
+
+    /**
+     * Constructs an InvalidCommand object with the specified error message.
+     *
+     * @param errorMessage the error message to be printed when the command is executed
+     */
     public InvalidCommand(String errorMessage) {
         this.errorMessage = errorMessage;
     }
+
+    /**
+     * Executes the invalid command by printing the error message to the console.
+     */
     @Override
     public void execute() {
         System.out.println(errorMessage);
